8eee63143d9bc0d2a0fc2fba7a61274d82bf9064
qudcks <qudcks0718@naver.com>
2026-03-12 21:07:57 +0900

    Init Commit

---
+++ Source/P_CAP/P_CAP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "P_CAP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P_CAP, "P_CAP" );
