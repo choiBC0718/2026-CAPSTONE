b18c652448097bdcf23e7393553061f3b3f6965d
qudcks <qudcks0718@naver.com>
2026-03-12 21:32:38 +0900

    기본 세팅 조금 추가

--- Source/P_CAP/P_CAP.Build.cs
+++ Source/P_CAP/P_CAP.Build.cs
@@ -8,9 +8,9 @@ public class P_CAP : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Paper2D", "Niagara", "StructUtils" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTasks", "GameplayTags", "UMG" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
